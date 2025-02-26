e7483d9fe740866df87dfc0eec0949b96ff790a5
Youssef Ait Rais <copybuisness007@gmail.com>
2025-02-26 10:24:23 +0100

    firstcommit

---
+++ src/main/java/code/Doctor/HelloServlet.java
@@ -0,0 +1,20 @@
+package code.Doctor;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    public HelloServlet() {
+        super();
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+}
